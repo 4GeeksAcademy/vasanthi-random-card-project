6d6962922118ffe50a57d0dc19619fba3c81ce08
dande100 <vasanthid10@gmail.com>
2023-07-17 19:26:35 -0500

    task: random card generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,50 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let suits = ["♠", "♣", "♥", "♦"];
+let values = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
+
+function randomValue() {
+  let randomValIndex = Math.floor(Math.random() * values.length);
+  return values[randomValIndex];
+}
+
+function randomSuits() {
+  let randomSuitsIndex = Math.floor(Math.random() * suits.length);
+  return suits[randomSuitsIndex];
+}
+
+function generateRandomCard() {
+  const genrateRandomVal = randomValue();
+  const genrateRandomSuit = randomSuits();
+  let suitsEl = document.querySelectorAll("#suits");
+  suitsEl.forEach(el => {
+    if (genrateRandomSuit == "♦" || genrateRandomSuit == "♥") {
+      el.style.color = "red";
+    } else {
+      el.style.color = "black";
+    }
+    el.innerHTML = genrateRandomSuit;
+  });
+  document.querySelector("#cardNo").innerHTML = genrateRandomVal;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.body.style.background = "green";
+  return generateRandomCard();
 };
+
+window.generateRandomCardOnClick = function generateRandomCardOnClick() {
+  return generateRandomCard();
+};
+
+window.generateRandomCardDimensions = function generateRandomCardDimensions() {
+  let widthVal = document.getElementById("width").value;
+  let heightVal = document.getElementById("height").value;
+  document.querySelector("#cardNo").style.height = heightVal + "px";
+  document.querySelector("#card").style.width = widthVal + "px";
+};
+
+setInterval(() => {
+  generateRandomCard();
+}, 10000);
